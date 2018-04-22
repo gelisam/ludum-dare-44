053b33229bfde5f378cd9ccdc55687b66007fd77
Samuel Gélineau <gelisam+github@gmail.com>
2018-04-22 19:23:42 -0400

    hush warnings

--- src/text.rs
+++ src/text.rs
@@ -8,6 +8,7 @@ fn text_size(text: &Text) -> Vector2 {
     Vector2::new(text.width() as f32, text.height() as f32)
 }
 
+#[allow(dead_code)]
 pub fn draw_centered_text(ctx: &mut Context, text: &Text, dest: Point2, rotation: f32) -> GameResult<()> {
     draw_centered(ctx, text, text_size(text), dest, rotation)
 }
