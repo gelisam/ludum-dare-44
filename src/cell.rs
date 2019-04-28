50ac669a309c8a4e092943ebc028e5e6f5168b4b
Samuel Gélineau <gelisam+github@gmail.com>
2019-04-27 22:26:26 -0400

    BranchCell, GiftCell

--- src/cell.rs
+++ src/cell.rs
@@ -1,15 +1,113 @@
-extern crate core;
-extern crate ggez;
-extern crate rand;
+use ggez::{GameResult, Context};
+use ggez::graphics::Image;
+
+use center;
+use hex::*;
+
 
 #[derive(Debug)]
-enum IntOrInfinite {
+pub struct Assets {
+    vert_branch_images: Vec<Vec<Image>>,
+    diag_branch_images: Vec<Vec<Image>>,
+    anti_diag_branch_images: Vec<Vec<Image>>,
+    leaf_images: Vec<Image>,
+    flower_images: Vec<Image>,
+    berry_images: Vec<Image>,
+    nut_images: Vec<Image>,
+}
+
+pub fn load_assets(ctx: &mut Context) -> GameResult<Assets> {
+    Ok(Assets {
+        vert_branch_images: vec!(
+            vec!(
+                Image::new(ctx, "/twig vert 1.png")?,
+                Image::new(ctx, "/twig vert 2.png")?,
+                Image::new(ctx, "/twig vert 3.png")?,
+            ),
+            vec!(
+                Image::new(ctx, "/branch vert 1.png")?,
+                Image::new(ctx, "/branch vert 2.png")?,
+                Image::new(ctx, "/branch vert 3.png")?,
+            ),
+            vec!(
+                Image::new(ctx, "/trunk vert 1.png")?,
+                Image::new(ctx, "/trunk vert 2.png")?,
+            ),
+        ),
+        diag_branch_images: vec!(
+            vec!(
+                Image::new(ctx, "/twig dia 1.png")?,
+                Image::new(ctx, "/twig dia 2.png")?,
+                Image::new(ctx, "/twig dia 3.png")?,
+            ),
+            vec!(
+                Image::new(ctx, "/branch dia 1.png")?,
+                Image::new(ctx, "/branch dia 2.png")?,
+            ),
+            vec!(
+                Image::new(ctx, "/trunk dia 1.png")?,
+                Image::new(ctx, "/trunk dia 2.png")?,
+            ),
+        ),
+        anti_diag_branch_images: vec!(
+            vec!(
+                Image::new(ctx, "/twig anti-dia 1.png")?,
+                Image::new(ctx, "/twig anti-dia 2.png")?,
+                Image::new(ctx, "/twig anti-dia 3.png")?,
+            ),
+            vec!(
+                Image::new(ctx, "/branch anti-dia 1.png")?,
+                Image::new(ctx, "/branch anti-dia 2.png")?,
+            ),
+            vec!(
+                Image::new(ctx, "/trunk anti-dia 1.png")?,
+                Image::new(ctx, "/trunk anti-dia 2.png")?,
+            ),
+        ),
+        leaf_images: vec!(
+            Image::new(ctx, "/leaves 1.png")?,
+            Image::new(ctx, "/leaves 2.png")?,
+        ),
+        flower_images: vec!(
+            Image::new(ctx, "/flower 1.png")?,
+            Image::new(ctx, "/flower 2.png")?,
+        ),
+        berry_images: vec!(
+            Image::new(ctx, "/flowers 3.png")?, // TODO: this is a flower, not a berry!
+        ),
+        nut_images: vec!(
+            Image::new(ctx, "/beehive.png")?, // TODO: this is a beehive, not a nut!
+        ),
+    })
+}
+
+impl Assets {
+    fn branch_images(&self, orientation: Orientation) -> &Vec<Vec<Image>> {
+        match orientation {
+            Orientation::Vert     => &self.vert_branch_images,
+            Orientation::Diag     => &self.diag_branch_images,
+            Orientation::AntiDiag => &self.anti_diag_branch_images,
+        }
+    }
+
+    fn gift_images(&self, gift: Gift) -> &Vec<Image> {
+        match gift {
+            Gift::Leaves  => &self.leaf_images,
+            Gift::Flowers => &self.flower_images,
+            Gift::Berries => &self.berry_images,
+            Gift::Nuts    => &self.nut_images,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub enum IntOrInfinite {
     Infinite,
     Finite(u32)
 }
 
-#[derive(Debug)]
-enum Gift {
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub enum Gift {
     Leaves,
     Flowers,
     Berries,
@@ -17,21 +115,107 @@ enum Gift {
 }
 
 #[derive(Debug)]
-struct Cell {
-	branch_strain_current: usize,
-    branch_upgrade: u8,
-    gift_upgrade: u8,
+pub struct GiftCell {
 	gift: Option<Gift>,
+    image_variant: usize,
+    parent: Option<BranchPoint>,
 }
 
-impl Cell {
+#[derive(Debug)]
+pub struct BranchCell {
+	branch_strain_current: usize,
+    branch_upgrade: usize,
+    image_variant: usize,
+    parent: Option<GiftPoint>,
+}
+
+impl BranchCell {
+    pub fn new() -> BranchCell {
+        BranchCell {
+            branch_strain_current: 0,
+            branch_upgrade: 0,
+            image_variant: 0,
+            parent: None,
+        }
+    }
+
     fn branch_strain_maximum(&self) -> IntOrInfinite {
         match self.branch_upgrade {
             0 => IntOrInfinite::Finite(5),
             1 => IntOrInfinite::Finite(25),
             2 => IntOrInfinite::Finite(125),
-            4 => IntOrInfinite::Infinite,
+            3 => IntOrInfinite::Infinite,
             _ => unreachable!(),
         }
+    }
+
+    pub fn next(&mut self, assets: &Assets, branch_point: BranchPoint) {
+        let n = assets.branch_images(branch_point.orientation())[self.branch_upgrade].len();
+        if self.image_variant+1 < n {
+            self.image_variant += 1;
+        } else {
+            self.image_variant = 0;
+            if self.branch_upgrade+1 < 3 {
+                self.branch_upgrade += 1;
+            } else {
+                self.branch_upgrade = 0;
+            }
+        }
+    }
+
+    pub fn draw(&self, ctx: &mut Context, assets: &Assets, branch_point: BranchPoint) -> GameResult<()> {
+        let image = &assets.branch_images(branch_point.orientation())[self.branch_upgrade][self.image_variant];
+        center::draw_centered_image(ctx, image, branch_point.to_point(), 0.0)
+    }
+}
+
+impl GiftCell {
+    pub fn new() -> GiftCell {
+        GiftCell {
+            gift: Some(Gift::Flowers),
+            image_variant: 0,
+            parent: None,
+        }
+    }
+
+    pub fn next(&mut self, assets: &Assets) {
+        if let Some(gift) = self.gift {
+            let n = assets.gift_images(gift).len();
+            if self.image_variant+1 < n {
+                self.image_variant += 1;
+            } else {
+                self.image_variant = 0;
+                self.gift = Some(
+                    match gift {
+                        Gift::Leaves  => Gift::Flowers,
+                        Gift::Flowers => Gift::Berries,
+                        Gift::Berries => Gift::Nuts,
+                        Gift::Nuts    => Gift::Leaves,
+                    }
+                );
+            }
+        }
+    }
+
+    pub fn draw(&self, ctx: &mut Context, assets: &Assets, gift_point: GiftPoint) -> GameResult<()> {
+        if let Some(gift) = self.gift {
+            let image = match gift {
+                Gift::Leaves => {
+                    &assets.leaf_images[self.image_variant]
+                },
+                Gift::Flowers => {
+                    &assets.flower_images[self.image_variant]
+                },
+                Gift::Berries => {
+                    &assets.berry_images[self.image_variant]
+                },
+                Gift::Nuts => {
+                    &assets.nut_images[self.image_variant]
+                },
+            };
+            center::draw_centered_image(ctx, image, gift_point.to_point(), 0.0)?;
+        }
+
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use ggez::timer;
 use std::collections::HashMap;
 
 mod bg;
+mod cell;
 mod center;
 mod channel;
 mod globals;
@@ -24,13 +25,10 @@ use globals::*;
 #[derive(Debug)]
 struct Assets {
     bg: bg::Assets,
+    cell: cell::Assets,
     dot: Mesh,
     font: Font,
     hex: hex::Assets,
-    vert_branch_images: Vec<Image>,
-    diag_branch_images: Vec<Image>,
-    anti_diag_branch_images: Vec<Image>,
-    gift_images: Vec<Image>,
 }
 
 impl Assets {
@@ -39,54 +37,11 @@ impl Assets {
 
         Ok(Assets {
             bg: bg::load_assets(ctx)?,
+            cell: cell::load_assets(ctx)?,
             dot: Mesh::new_circle(ctx, DrawMode::Fill, Point2::new(0.0, 0.0), 10.0, 3.0)?,
             font,
             hex: hex::load_assets(ctx)?,
-            vert_branch_images: vec!(
-                Image::new(ctx, "/twig vert 1.png")?,
-                Image::new(ctx, "/twig vert 2.png")?,
-                Image::new(ctx, "/twig vert 3.png")?,
-                Image::new(ctx, "/branch vert 1.png")?,
-                Image::new(ctx, "/branch vert 2.png")?,
-                Image::new(ctx, "/branch vert 3.png")?,
-                Image::new(ctx, "/trunk vert 1.png")?,
-                Image::new(ctx, "/trunk vert 2.png")?,
-            ),
-            diag_branch_images: vec!(
-                Image::new(ctx, "/twig dia 1.png")?,
-                Image::new(ctx, "/twig dia 2.png")?,
-                Image::new(ctx, "/twig dia 3.png")?,
-                Image::new(ctx, "/branch dia 1.png")?,
-                Image::new(ctx, "/branch dia 2.png")?,
-                Image::new(ctx, "/trunk dia 1.png")?,
-                Image::new(ctx, "/trunk dia 2.png")?,
-            ),
-            anti_diag_branch_images: vec!(
-                Image::new(ctx, "/twig anti-dia 1.png")?,
-                Image::new(ctx, "/twig anti-dia 2.png")?,
-                Image::new(ctx, "/twig anti-dia 3.png")?,
-                Image::new(ctx, "/branch anti-dia 1.png")?,
-                Image::new(ctx, "/branch anti-dia 2.png")?,
-                Image::new(ctx, "/trunk anti-dia 1.png")?,
-                Image::new(ctx, "/trunk anti-dia 2.png")?,
-            ),
-            gift_images: vec!(
-                Image::new(ctx, "/leaves 1.png")?,
-                Image::new(ctx, "/leaves 2.png")?,
-                Image::new(ctx, "/flower 1.png")?,
-                Image::new(ctx, "/flower 2.png")?,
-                Image::new(ctx, "/flowers 3.png")?,
-                Image::new(ctx, "/beehive.png")?,
-            ),
         })
-    }
-
-    fn branch_images(&self, orientation: hex::Orientation) -> &Vec<Image> {
-        match orientation {
-            hex::Orientation::Vert     => &self.vert_branch_images,
-            hex::Orientation::Diag     => &self.diag_branch_images,
-            hex::Orientation::AntiDiag => &self.anti_diag_branch_images,
-        }
     }
 }
 
@@ -101,8 +56,8 @@ struct Globals {
     bounty_amount: f32,
     life_amount: f32,
     hover: Option<hex::InBoundsPoint>,
-    branches: HashMap<hex::BranchPoint, usize>,
-    gifts: HashMap<hex::GiftPoint, usize>,
+    branches: HashMap<hex::BranchPoint, cell::BranchCell>,
+    gifts: HashMap<hex::GiftPoint, cell::GiftCell>,
 }
 
 impl Globals {
@@ -124,7 +79,8 @@ impl Globals {
         )?;
 
         let mut branches = HashMap::with_capacity(100);
-        branches.insert(hex::BranchPoint::new(hex::HexPoint::new(0, 1)), 7);
+        let root = cell::BranchCell::new();
+        branches.insert(hex::BranchPoint::new(hex::HexPoint::new(0, 1)), root);
 
         Ok(Globals {
             assets,
@@ -189,24 +145,18 @@ impl EventHandler for Globals {
                 MouseButton::Left => {
                     match in_bounds_point {
                         hex::InBoundsPoint::BranchPoint(branch_point) => {
-                            match self.branches.get(&branch_point).map (|x| *x) {
-                                None => {
-                                    self.branches.insert(branch_point, 0);
-                                },
-                                Some(i) => {
-                                    self.branches.insert(branch_point, (i + 1) % self.assets.branch_images(branch_point.orientation()).len());
-                                },
-                            }
+                            let assets_ = &mut self.assets;
+                            self.branches
+                                .entry(branch_point)
+                                .and_modify(|b| b.next(&assets_.cell, branch_point))
+                                .or_insert(cell::BranchCell::new());
                         },
                         hex::InBoundsPoint::GiftPoint(gift_point) => {
-                            match self.gifts.get(&gift_point).map (|x| *x) {
-                                None => {
-                                    self.gifts.insert(gift_point, 0);
-                                },
-                                Some(i) => {
-                                    self.gifts.insert(gift_point, (i + 1) % self.assets.gift_images.len());
-                                },
-                            }
+                            let assets_ = &mut self.assets;
+                            self.gifts
+                                .entry(gift_point)
+                                .and_modify(|g| g.next(&assets_.cell))
+                                .or_insert(cell::GiftCell::new());
                         },
                     }
                 },
@@ -241,13 +191,11 @@ impl EventHandler for Globals {
         self.life.draw(ctx)?;
 
         set_color(ctx, Color::from_rgb(255, 255, 255))?;
-        for (hex_point, branch_index) in self.branches.iter() {
-            let image = &self.assets.branch_images(hex_point.orientation())[*branch_index];
-            center::draw_centered_image(ctx, image, hex_point.to_point(), 0.0)?;
+        for (&branch_point, branch_cell) in self.branches.iter() {
+            branch_cell.draw(ctx, &self.assets.cell, branch_point)?;
         }
-        for (hex_point, gift_index) in self.gifts.iter() {
-            let image = &self.assets.gift_images[*gift_index];
-            center::draw_centered_image(ctx, image, hex_point.to_point(), 0.0)?;
+        for (&gift_point, gift_cell) in self.gifts.iter() {
+            gift_cell.draw(ctx, &self.assets.cell, gift_point)?;
         }
 
         if let Some(in_bounds_point) = self.hover {
